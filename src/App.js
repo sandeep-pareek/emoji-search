25a05cf08fc2cdb7d4137875f379f81c776b48f4
Sandeep pareek <psandeep01@gmail.com>
2019-03-08 19:35:06 +0530

    Updated default size to 100

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      filteredEmoji: filterEmoji("", 20)
+      filteredEmoji: filterEmoji("", 100)
     };
   }
 
